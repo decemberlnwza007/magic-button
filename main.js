4835afc897993107cb468e9cf022544a679387fa
Pongsakorn Thongrak <saraythxngraks25@gmail.com>
2024-10-24 16:14:37 +0700

    add magic

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-function handleSubmit(){
-    try{
+function handleSubmit() {
+    try {
         const input = document.getElementById('input');
         const result = document.getElementById('result');
 
@@ -7,7 +7,26 @@ function handleSubmit(){
         result.innerHTML = input.value;
 
         console.log(input.value);
-    }catch(error){
+    } catch (error) {
         console.error(error);
     }
 }
+
+const websites = [
+    'https://www.google.com',
+    'https://www.facebook.com',
+    'https://www.youtube.com',
+    'https://www.wikipedia.org',
+    'https://www.amazon.com',
+    'https://www.reddit.com',
+    'https://www.netflix.com',
+    'https://www.twitter.com',
+    'https://www.instagram.com',
+    'https://www.linkedin.com'
+];
+
+document.getElementById('randomButton').addEventListener('click', function() {
+    const randomIndex = Math.floor(Math.random() * websites.length);
+    const randomWebsite = websites[randomIndex];
+    window.location.href = randomWebsite;
+})
